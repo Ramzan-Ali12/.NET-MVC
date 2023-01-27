bcd00c637b0bbfe66c6e7d90894b61efad607809
M Ramzan Ali <103037323+Ramzan-Ali12@users.noreply.github.com>
2023-01-27 19:08:48 +0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Simple> Employee { get; set; }
+
+    }
+}
